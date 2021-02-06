55d1fa0a3e38dd51970cad4d4488d2f61a2dd05a
Milo Moisson <milomoisson@gmail.com>
2021-02-06 19:29:21 +0100

    fix: modify some configs

--- fish/config.fish
+++ fish/config.fish
@@ -1,4 +1,4 @@
-set -gp fish_user_paths '/usr/local/sbin' '~/.yarn/bin' '~/.config/yarn/global/node_modules/.bin'
+set -gp PATH '/usr/local/sbin' '~/.yarn/bin' '~/.config/yarn/global/node_modules/.bin'
 set GPG_TTY (tty)
 
 starship init fish | source
