7c3df49f0e8428cbd0cbb6328e7b768afb4d6cb4
Milo Moisson <milomoisson@gmail.com>
2021-03-12 20:08:43 +0100

    feat: add fish !! key binding

---
+++ fish/functions/double_bang_bind.fish
@@ -0,0 +1,9 @@
+# Defined interactively
+function double_bang_bind
+    switch (commandline -t)[-1]
+        case "!"
+            commandline -t $history[1]; commandline -f repaint
+        case "*"
+            commandline -i !
+    end
+end
---
+++ fish/functions/fish_user_key_bindings.fish
@@ -0,0 +1,5 @@
+# Defined interactively
+function fish_user_key_bindings
+    bind ! double_bang_bind
+    bind \cd exit
+end
