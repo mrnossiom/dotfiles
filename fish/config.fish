07aec993d59c3db9246357709d8b91b9da37af88
Milo Moisson <milomoisson@gmail.com>
2021-04-03 12:18:58 +0200

    fix: 🐛 config
    GPG_TTY is also useful on linux

--- fish/config.fish
+++ fish/config.fish
@@ -2,12 +2,15 @@ set platform (uname)
 
 # For Mac
 if test $platform = 'Darwin'
-	set -g GPG_TTY (tty)
+	# Nothing here yet...
 # For Linux (or WSL)
 else if test $platform = 'Linux'
 	if test pwd = '/mnt/c/Users/milom'; cd ~; end
 	test -d ~/.linuxbrew && eval (~/.linuxbrew/bin/brew shellenv)
 	test -d /home/linuxbrew/.linuxbrew && eval (/home/linuxbrew/.linuxbrew/bin/brew shellenv)
 end
+
+# Common to all platforms
+set -g GPG_TTY (tty)
 
 starship init fish | source
