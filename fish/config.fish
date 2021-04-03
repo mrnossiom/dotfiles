7eca83c82125b18e53f369e35de1efbf5e0cd1a3
Milo Moisson <milomoisson@gmail.com>
2021-04-03 11:49:29 +0200

    feat: add linux to the project

--- fish/config.fish
+++ fish/config.fish
@@ -1,3 +1,13 @@
-set -gx GPG_TTY (tty)
+set platform (uname)
+
+# For Mac
+if test $platform = 'Darwin'
+	set -g GPG_TTY (tty)
+# For Linux (or WSL)
+else if test $platform = 'Linux'
+	if test pwd = '/mnt/c/Users/milom'; cd ~; end
+	test -d ~/.linuxbrew && eval (~/.linuxbrew/bin/brew shellenv)
+	test -d /home/linuxbrew/.linuxbrew && eval (/home/linuxbrew/.linuxbrew/bin/brew shellenv)
+end
 
 starship init fish | source
