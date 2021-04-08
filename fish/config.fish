f7e0fca0364bc26403d40659624eef18118482c4
Milo Moisson <milomoisson@gmail.com>
2021-04-08 17:13:39 +0200

    feat: update config, ssh hosts and env

--- fish/config.fish
+++ fish/config.fish
@@ -1,15 +1,25 @@
+# Init
 set platform (uname)
+
+# Common to all platforms
+set -gx GPG_TTY (tty)
+
+set fish_user_paths \
+	$HOME/.config/yarn/global/node_modules/.bin/ \
+	$HOME/.cargo/bin/
 
 # For Mac
 if test $platform = 'Darwin'
 	# Nothing here yet...
-# For Linux (or WSL)
+# For Linux 
 else if test $platform = 'Linux'
 	test -d ~/.linuxbrew && eval (~/.linuxbrew/bin/brew shellenv)
 	test -d /home/linuxbrew/.linuxbrew && eval (/home/linuxbrew/.linuxbrew/bin/brew shellenv)
-end
 
-# Common to all platforms
-set -gx GPG_TTY (tty)
+	# For WSL
+	if string match -r WSL (uname -r)
+		# Nothing here yet...
+	end
+end
 
 starship init fish | source
