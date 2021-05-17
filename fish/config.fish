845e689c8c6f932fce073b427204fe1323a09af3
Milo Moisson <milomoisson@gmail.com>
2021-05-17 14:25:47 +0200

    feat: upgrade and add some deps

--- fish/config.fish
+++ fish/config.fish
@@ -5,21 +5,24 @@ set platform (uname)
 set -gx GPG_TTY (tty)
 
 set fish_user_paths \
-	$HOME/.config/yarn/global/node_modules/.bin/ \
-	$HOME/.cargo/bin/
+    $HOME/.config/yarn/global/node_modules/.bin/ \
+    $HOME/.cargo/bin/ \
+    /usr/local/sbin
+
+fish_add_path /usr/local/opt/curl/bin
 
 # For Mac
-if test $platform = 'Darwin'
-	# Nothing here yet...
-# For Linux 
-else if test $platform = 'Linux'
-	test -d ~/.linuxbrew && eval (~/.linuxbrew/bin/brew shellenv)
-	test -d /home/linuxbrew/.linuxbrew && eval (/home/linuxbrew/.linuxbrew/bin/brew shellenv)
+if test $platform = Darwin
+    # Nothing here yet...
+    # For Linux 
+else if test $platform = Linux
+    test -d ~/.linuxbrew && eval (~/.linuxbrew/bin/brew shellenv)
+    test -d /home/linuxbrew/.linuxbrew && eval (/home/linuxbrew/.linuxbrew/bin/brew shellenv)
 
-	# For WSL
-	if string match -r WSL (uname -r)
-		# Nothing here yet...
-	end
+    # For WSL
+    if string match -r WSL (uname -r)
+        # Nothing here yet...
+    end
 end
 
 starship init fish | source
