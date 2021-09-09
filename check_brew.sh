f05099681e6f45a4d4532b0225bff3ced92f4974
Milo Moisson <milomoisson@gmail.com>
2021-09-09 22:07:36 +0200

    preparing for codespaces three

--- check_brew.sh
+++ check_brew.sh
@@ -5,5 +5,6 @@ if ! command -v brew &> /dev/null
 then
     echo "Brew wasn't found. Tring to install"
     /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+    eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv)"
     exit
 fi
