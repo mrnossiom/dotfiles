12fa1ae3617117dc019fb867ce75835ccc037e80
Milo Moisson <milomoisson@gmail.com>
2022-03-18 20:31:28 +0100

    chore: update deps and update brewfile

--- fish/functions/fish_greeting.fish
+++ fish/functions/fish_greeting.fish
@@ -4,5 +4,5 @@ function fish_greeting
 	if test (pwd) = '/mnt/c/Users/milom'; cd ~; end
 	
 	echo 'Hello '(set_color brblue)(whoami)(set_color normal)' you are on '(set_color brred)$platform(set_color normal)'.'
-	echo 'Your current directory is '(set_color brgreen)(pwd)
+	echo 'Your current directory is '(set_color brgreen)(pwd)(set_color normal)
 end
