a781d64523455d48197d1f0acb530bb9487a9fb7
Milo Moisson <milomoisson@gmail.com>
2022-03-22 21:06:24 +0100

    fix: rust update script when no deps to upgrade

--- scripts/rust_deps_update.sh
+++ scripts/rust_deps_update.sh
@@ -6,11 +6,11 @@ cd $HOME
 
 # If the .crates.toml file doesn't exist, exit with code 1
 if [ ! -s ".cargo/.crates.toml" ]; then
+	echo "No .crates.toml file found. Exiting."
 	exit 1
 fi
 
-# Install all the missing binaries
-cargo install-update -f $(
+CRATES_TO_UPDATE=$(
 	# Dynamically create a script with awk to check if the corresponding binary exists
 	awk -F= '/\((registry|git)\+/ {
 		sub(/"/, "", $1);
@@ -18,8 +18,16 @@ cargo install-update -f $(
 		gsub(/[ "\[\]]/, "", $2);
 		sub(/,$/, "", $2); split($2, b, ",");
 		for(i = 1; i <= length(b); ++i) print "[ -x ~/.cargo/bin/" b[i] " ] || echo " $1
-	}' .cargo/.crates.toml |
+	}' ~/.cargo/.crates.toml |
 
 	# Execute the script and filter duplicates if any
 	sh | uniq
 )
+
+# Install all the missing binaries if any
+if [ $(echo -n $CRATES_TO_UPDATE | wc -c) -ne 0 ]; then
+	cargo install-update --force $CRATES_TO_UPDATE
+else
+	echo "No crates to update"
+fi
+
