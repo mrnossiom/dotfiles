791253b435a680d88dcea33f92fd3b383b98c8d7
Milo Moisson <milomoisson@gmail.com>
2022-10-05 18:15:27 +0200

    feat: add many Brew packages

--- fish/config.fish
+++ fish/config.fish
@@ -1,3 +1,5 @@
+# TODO: ingore the `fish_variables` file and add abbr in here
+
 # Init
 set platform (uname)
 
@@ -5,22 +7,28 @@ set platform (uname)
 set -gx GPG_TTY (tty)
 
 set fish_user_paths \
+    $HOME/.bun/bin \
     $HOME/.config/yarn/global/node_modules/.bin/ \
     $HOME/.cargo/bin/ \
+    $HOME/.local/bin \
     /usr/local/bin \
     /usr/local/sbin \
-    /home/milomoisson/.local/bin
+    /usr/local/opt/curl/bin
 
-fish_add_path /usr/local/opt/curl/bin
+eval /home/milomoisson/miniconda3/bin/conda "shell.fish" hook $argv | source
 
 # For Mac
 if test $platform = Darwin
     # Nothing here yet.
 
-# For Linux
+    # For Linux
 else if test $platform = Linux
+    # Brew env setup
     test -d ~/.linuxbrew && eval (~/.linuxbrew/bin/brew shellenv)
     test -d /home/linuxbrew/.linuxbrew && eval (/home/linuxbrew/.linuxbrew/bin/brew shellenv)
+
+    # Discord presence fix
+    ln -sf {app/com.discordapp.Discord,$XDG_RUNTIME_DIR}/discord-ipc-0
 
     # For WSL
     if string match -r WSL (uname -r)
