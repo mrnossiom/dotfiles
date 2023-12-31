faf09ef93b5b3e783a6762e4a721742c140e846b
Milo Moisson <milomoisson@gmail.com>
2023-12-31 10:29:27 +0100

    feat: rekey secrets with new neo laptop ssh key and remove old-neo

--- secrets/secrets.nix
+++ secrets/secrets.nix
@@ -1,7 +1,7 @@
 let
-  old-neo = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAINUkJvLMjjzbZSrnucc2uQeRhiuXPiZXNjqT80PVSSQb";
   archaic = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIHLLJ+6UiJYTD0HhWwTBom5fmZ4RaCXAUgcGaXgfdG8S";
-  systems = [ old-neo archaic ];
+  neo = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIOelV3jIbukHfMJB+oRxjzy+O3RIPD3AIdgqI9gIPEHe";
+  systems = [ archaic neo ];
 in
 {
   "ca5e.pgp.age".publicKeys = systems;
