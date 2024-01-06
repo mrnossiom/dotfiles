af9d2f7f478a62dfcbb76750d2061845f46fde57
Milo Moisson <milomoisson@gmail.com>
2024-01-06 19:30:58 +0100

    chore: rekeyed secrets with new encrypted ssh key

--- secrets/secrets.nix
+++ secrets/secrets.nix
@@ -1,6 +1,9 @@
 let
   archaic = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIHLLJ+6UiJYTD0HhWwTBom5fmZ4RaCXAUgcGaXgfdG8S";
-  neo = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIOelV3jIbukHfMJB+oRxjzy+O3RIPD3AIdgqI9gIPEHe";
+
+  # Password protected keys 
+  neo = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIJdt7atyPTOfaBIsgDYYb0DG1yid2u78abaCDji6Uxgi";
+
   systems = [ archaic neo ];
 in
 {
