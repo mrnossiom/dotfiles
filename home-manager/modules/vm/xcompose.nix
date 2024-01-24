b1415a6b39c227b1404eb2b6b7831fa58430cd56
Milo Moisson <milomoisson@gmail.com>
2024-01-24 16:16:59 +0100

    feat: add first few custom compose key sequence to progressively get rid of default

--- home-manager/modules/vm/xcompose.nix
+++ home-manager/modules/vm/xcompose.nix
@@ -5,23 +5,71 @@
 , ...
 }:
 
+with lib;
+
 let
   inherit (self.outputs) homeManagerModules;
 in
 {
   imports = [ homeManagerModules.xcompose ];
 
-  options = { };
-
   config.programs.xcompose = {
     enable = true;
     includeLocaleCompose = true;
+    loadConfigInEnv = false;
 
-    sequences = {
-      Multi_key = {
-        comma = "̧";
-        h.i = "helo";
+    sequences.Multi_key = {
+      e.grave = "è";
+      e.apostrophe = "é";
+      a.grave = "à";
+      u.grave = "ù";
+
+      # Lower case [g]reek letters
+      g = {
+        a = "α";
+        b = "β";
+        g = "γ";
+        d = "δ";
+        e = "ε";
+        z = "ζ";
+        h = "η"; # ?
+        # u = "θ"; # ?
+        i = "ι";
+        k = "κ";
+        l = "λ";
+        m = "μ";
+        n = "ν";
+        x = "ξ";
+        o = "ο";
+        p = "π";
+        r = "ρ";
+        s = "σ";
+        t = "τ";
+        u = "υ";
+        f = "φ";
+
       };
+      # Lower case [G]reek letters
+      G = { };
+
+      # Math
+      l.equal = "≤";
+      g.equal = "≥";
+      s.u.m = "∑";
+
+      # Symbols
+      o.o = "∞";
+
+      minus.greater = "→";
+      less.minus = "←";
+      less.greater.minus = "↔";
+
+      equal.greater = "⇒";
+      less.equal = "⇐";
+      less.greater.equal = "⇔";
+
+      "0"."0" = "°";
+      minus.minus = "—";
     };
   };
 }
