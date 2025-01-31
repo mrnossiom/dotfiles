706b46333b8d80236f7fd02d8ed3d2903aac78f5
Milo Moisson <milomoisson@gmail.com>
2025-01-31 14:07:29 +0100

    Back out "feat(fish): always use zellij default session"
    This backs out commit 9694bf19ad42f941b54fcd5ec403a22a120bec4b.

--- home-manager/fragments/shell.nix
+++ home-manager/fragments/shell.nix
@@ -69,12 +69,6 @@ in
 
       interactiveShellInit = ''
         abbr -a !! --position anywhere --function last_history_item
-
-        if type -q zellij
-          if not test -n "$ZELLIJ"
-            exec zellij attach default; or exec zellij --session default
-          end
-        end
       '';
 
       shellAliases = {
