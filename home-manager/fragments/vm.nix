b3f951c55dc62a2f3df46462d72b6ed504c77ba9
Milo Moisson <milomoisson@gmail.com>
2025-02-21 19:50:37 +0100

    fix(vm): change clear cursor trigger

--- home-manager/fragments/vm.nix
+++ home-manager/fragments/vm.nix
@@ -238,11 +238,8 @@ in
         seat = {
           "seat0" = {
             xcursor_theme = "Bibata-Modern-Ice";
-            # Clears cursor after 5s
-            hide_cursor = toString (5 * 1000);
-            # Workaround, because key cannot be included twice
-            # disable cursor when typing
-            "" = "hide_cursor when-typing enable";
+            # disable cursor when typing or on purpose
+            hide_cursor = "when-typing enable";
           };
         };
 
