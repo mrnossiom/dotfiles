b0b4d1c1b52bb031384c3dd4230d183f97060a97
Milo Moisson <milomoisson@gmail.com>
2025-04-03 15:01:12 +0200

    refactor: remove broken `createFragment`

--- lib/default.nix
+++ lib/default.nix
@@ -1,13 +1,4 @@
 # This flake library is available to modules via the `llib` arg
-pkgs: with pkgs.lib; {
+pkgs: {
   colorSchemes = import ./colorSchemes.nix;
-
-  createFragment = name: config: {
-    options."${name}".enable = mkOption {
-      description = "Whether to enable `${name}` fragment";
-      type = types.bool;
-    };
-
-    config = mkIf true config;
-  };
 }
