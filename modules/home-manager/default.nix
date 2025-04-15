85faf29ce8dcbd53c5f7e52cc864235f952d9f6b
Milo Moisson <milo@wiro.world>
2025-04-16 01:33:58 +0200

    feat: add protonmail-bridge hm module from nixpkgs module

--- modules/home-manager/default.nix
+++ modules/home-manager/default.nix
@@ -1,6 +1,7 @@
 {
-  color-scheme = import ./color-scheme.nix;
-  wakatime = import ./wakatime.nix;
-  wl-clip-persist = import ./wl-clip-persist.nix;
-  xcompose = import ./xcompose.nix;
+  color-scheme = ./color-scheme.nix;
+  protonmail-bridge = ./protonmail-bridge.nix;
+  wakatime = ./wakatime.nix;
+  wl-clip-persist = ./wl-clip-persist.nix;
+  xcompose = ./xcompose.nix;
 }
