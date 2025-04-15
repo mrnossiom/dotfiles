424bf8979c99bcb730c7dcc6a3a391c83091ff5a
Milo Moisson <milo@wiro.world>
2025-04-16 01:33:58 +0200

    feat: add protonmail-bridge hm module from nixpkgs module

---
+++ modules/home-manager/protonmail-bridge.nix
@@ -0,0 +1,59 @@
+# Taken from the nixpkgs repository
+
+{ config
+, lib
+, pkgs
+, ...
+}:
+
+let
+  cfg = config.services.protonmail-bridge;
+in
+{
+  options.services.protonmail-bridge = {
+    enable = lib.mkEnableOption "protonmail bridge";
+
+    package = lib.mkPackageOption pkgs "protonmail-bridge" { };
+
+    # path = lib.mkOption {
+    #   type = lib.types.listOf lib.types.path;
+    #   default = [ ];
+    #   example = lib.literalExpression "with pkgs; [ pass gnome-keyring ]";
+    #   description = "List of derivations to put in protonmail-bridge's path.";
+    # };
+
+    logLevel = lib.mkOption {
+      type = lib.types.nullOr (
+        lib.types.enum [ "panic" "fatal" "error" "warn" "info" "debug" ]
+      );
+      default = null;
+      description = "Log level of the Proton Mail Bridge service. If set to null then the service uses it's default log level.";
+    };
+  };
+
+  config = lib.mkIf cfg.enable {
+    systemd.user.services.protonmail-bridge = {
+      Unit = {
+        Description = "protonmail bridge";
+        After = [ "graphical-session.target" ];
+      };
+
+      Install.WantedBy = [ "graphical-session.target" ];
+
+      Service =
+        let
+          logLevel = lib.optionalString (cfg.logLevel != null) "--log-level ${cfg.logLevel}";
+        in
+        {
+          ExecStart = "${lib.getExe cfg.package} --noninteractive ${logLevel}";
+          Restart = "always";
+        };
+
+      # path = cfg.path;
+    };
+
+    home.packages = [ cfg.package ];
+  };
+
+  meta.maintainers = [ "mrnossiom" ];
+} 
