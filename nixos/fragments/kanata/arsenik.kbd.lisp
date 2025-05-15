963eee322dec568868bf4653debe8af065271d5b
Milo Moisson <milo@wiro.world>
2025-05-15 19:43:27 +0200

    feat(kanata): final cleaup with addition of lafayette layer for qwerty

--- nixos/fragments/kanata/arsenik.kbd.lisp
+++ nixos/fragments/kanata/arsenik.kbd.lisp
@@ -10,19 +10,7 @@
   long_hold_timeout 200
 )
 
- 
-;; no-angle-mod
-
-;; (defsrc
-;;   1    2    3    4    5  bspc   6    7    8    9    0
-;;   q    w    e    r    t         y    u    i    o    p
-;;   a    s    d    f    g         h    j    k    l    ;
-;;   z    x    c    v    b    <    n    m    ,    .    /
-;;             lalt          spc             ralt
-;; )
-
 ;; angle-mod: the ISO key (a.k.a. LSGT or 102 key) becomes Z
-
 (defsrc
   1    2    3    4    5   bspc  6    7    8    9    0
   q    w    e    r    t         y    u    i    o    p
@@ -32,21 +20,21 @@
 )
 
 ;; Base layer: layer-taps under the thumbs + home-row mods on SDF and JKL
-
 (deflayer base
   _    _    _    _    _   _    _    _    _    _    _
   q    w    e    r    t        y    u    i    o    p
   @aa  @ss  @dd  @ff  g        h    @jj  @kk  @ll  @semisft
   z    x    c    v    b   <    n    m    ,    .    /
-       ∅    @num          @nav           @sym ∅
+       _    @num          @nav           @sym _
 )
 
 ;; Timing variables are defined in `kanata.kbd` file.
-
 (defalias
   nav (tap-hold $tap_timeout $long_hold_timeout spc (layer-while-held navigation))
   num (tap-hold-press $tap_timeout $hold_timeout bspc (layer-while-held numrow))
   sym (tap-hold-press $tap_timeout $hold_timeout ret (layer-while-held symbols))
+
+  semi ;
 
   ;; Home-row mods
   aa (tap-hold $tap_timeout $long_hold_timeout a lsft)
@@ -56,72 +44,67 @@
   jj (tap-hold $tap_timeout $long_hold_timeout j rctl)
   kk (tap-hold $tap_timeout $long_hold_timeout k rmet)
   ll (tap-hold $tap_timeout $long_hold_timeout l ralt)
-  semisft (tap-hold $tap_timeout $long_hold_timeout ; rsft)
+  semisft (tap-hold $tap_timeout $long_hold_timeout @semi rsft)
 )
 
 ;; Symbol layer: same as AltGr but enables a NumRow.
-;; Concretely this does nothing, just let AltGr as-is for keyboard layouts
-;; depending heavily on AltGr, like Bépo or simply Lafayette layouts like
-;; Ergo‑L, which already have that layer baked in.
-;; Except it adds an NumRow layer.
-
 (deflayer symbols
-  AG-1 AG-2 AG-3 AG-4 AG-5 XX     AG-6 AG-7 AG-8 AG-9 AG-0
-  AG-q AG-w AG-e AG-r AG-t        AG-y AG-u AG-i AG-o AG-p
-  AG-a AG-s AG-d AG-f AG-g        AG-h AG-j AG-k AG-l AG-;
-  AG-z AG-x AG-c AG-v AG-b AG-<   AG-n AG-m AG-, AG-. AG-/
-       _    _              AG-spc           _    _
+  AG-1 AG-2 AG-3 AG-4 AG-5 XX   AG-6 AG-7 AG-8 AG-9 AG-0
+  @^   @<   @>   @$   @%        @@   @&   @*   @'   @`
+  @{   @pl  @pr  @}   @=        @\   @+   @-   @/   @''
+  @~   @[   @]   @_   @#   XX   @|   @!   @;   @:   @?
+       XX   XX            spc             XX   XX
 )
 
 ;; Numrow layer
-
 (deflayer numrow
-  _    _    _    _    _    _    _    _    _    _    _
-  _    _    _    _    _         _    _    _    _    _  
+  XX   XX   XX   XX   XX   XX   XX   XX   XX   XX   XX
+  XX   XX   XX   XX   XX        XX   XX   XX   XX   XX  
   @1   @2   @3   @4   @5        @6   @7   @8   @9   @0
-  _    _    _    _    _    _    _    _    _    _    _
-       _    _              @nbs           _    _
+  XX   XX   XX   XX   XX   XX   XX   XX   XX   XX   XX
+       XX   XX             XX             XX   XX
 )
 
 ;; Vim-Navigation layer:
 ;;  - right: Vim-like arrows on HJKL, home/end page up/down, mouse scroll
 ;;  - left: one-hand shortcuts (Cmd/Ctrl-WASZXCV), Tab/Shift-Tab, prev/next
 ;;  - top: Super-num (i3/sway) or Alt-num (browser), zoom in/out
-
-;; The `lrld` action stands for "live reload". This will re-parse everything
-;; except for linux-dev, i.e. you cannot live reload and switch keyboard devices.
-
 (deflayer navigation
-  M-1  M-2  M-3  M-4  M-5  lrld M-6  M-7  M-8  M-9  M-0
-  @pad @cls bck  fwd  XX        home pgdn pgup end  @run
-  @all @sav S-tab tab XX        lft  down up   rght @fun
-  @ndo @cut @cpy @pst XX   _    @mwl @mwd @mwu @mwr XX
-       _    del            _              esc  _
+  M-1  M-2  M-3   M-4  M-5 XX   M-6  M-7  M-8  M-9  M-0
+  @pad @cls bck   fwd  XX       home pgdn pgup end  @run
+  @all @sav S-tab tab  XX       lft  down up   rght @fun
+  @ndo @cut @cpy  @pst XX  XX   @mwl @mwd @mwu @mwr XX
+       XX   del            XX             esc  XX
 )
 
 ;; NumPad
 (deflayer numpad
-  _    _    _    _    _    _    _    _    _    _    _
+  XX   XX   XX   XX   XX   XX   XX   XX   XX   XX   XX
   XX   home up   end  pgup      @/   @7   @8   @9   XX
   XX   lft  down rght pgdn      @-   @4   @5   @6   @0
-  XX   XX   XX   XX   XX   _    @,   @1   @2   @3   @.
-       _    @std           @nbs           @std _
+  XX   XX   XX   XX   XX   XX   @,   @1   @2   @3   @.
+       XX   @std           XX             @std ∅
 )
 
 ;; function keys
 (deflayer funpad
-  XX   XX   XX   XX   XX   XX   XX   XX   XX   XX   XX
-  f1   f2   f3   f4   XX        XX   XX   XX   XX   XX
-  f5   f6   f7   f8   XX        XX   lctl lalt lmet _
-  f9   f10  f11  f12  XX   XX   XX   XX   XX   XX   XX
-       _    _               _             _    _
+  XX   XX   XX   XX   XX     XX  XX       XX   XX   XX   XX
+  f1   f2   f3   f4   @volup     @brghtup XX   XX   XX   XX
+  f5   f6   f7   f8   @voldn     @brghtdn lctl lalt lmet XX
+  f9   f10  f11  f12  XX     XX  XX       XX   XX   XX   XX
+       XX   @std             XX                @std XX
 )
 
 (defalias
   std (layer-switch base)
   pad (layer-switch numpad)
+  fun (layer-switch funpad)
 
-  fun (layer-while-held funpad)
+  ;; TODO: find how to implement these with XF86 keys
+  volup XX
+  voldn XX
+  brghtup XX
+  brghtdn XX
 
   ;; Mouse wheel emulation
   mwu (mwheel-up    50 120)
@@ -133,7 +116,6 @@
 ;; Key of the application launcher, mapped to P(nav)
 (defalias run M-d)
 
-;; Aliases for `Symbols` and `Navigation` layers
 ;; Qwerty/Colemak Windows/Linux aliases
 ;; Works with QWERTY-US, Colemak and others.
 
@@ -195,26 +177,4 @@
   ;  ;
   :  S-;
   ?  S-/
-)
-
-;; NumRow layer
-(defalias
-  s0 S-0
-  s1 S-1
-  s2 S-2
-  s3 S-3
-  s4 S-4
-  s5 S-5
-  s6 S-6
-  s7 S-7
-  s8 S-8
-  s9 S-9
-  nbs spc ;; no narrow no-break space in Qwerty
-
-  dk1 XX
-  dk2 XX
-  dk3 XX
-  dk4 XX
-
-  dk5 XX
 )
