c42f7c260b007713806b8b78fd974f46257b3b3b
Milo Moisson <milo@wiro.world>
2026-03-09 17:45:02 +0000

    swaylock: update fork to v1.8.5

--- pkgs/swaylock.nix
+++ pkgs/swaylock.nix
@@ -9,7 +9,7 @@ swaylock.overrideAttrs (old: {
   src = fetchFromGitHub {
     owner = "mrnossiom";
     repo = "swaylock";
-    rev = "1e949610081ea0788d9fba6f0d7c909d7b62e9e0";
-    hash = "sha256-3YN6n5mYB7r1Xk22AGYMusNbA6aBD6OMU3Gn9OOuS6o=";
+    rev = "c1f213e8ba96706e9f42963941850ffb51bd9e53";
+    hash = "sha256-1OeReYEJv66fHUiHL0B1+H0aQNqWlY2ZauFVyAAMNTo=";
   };
 })
