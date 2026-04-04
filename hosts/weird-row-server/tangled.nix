3ea5eb4a1fc25b5214a9c566d385b26dcd199a6e
Milo Moisson <milo@wiro.world>
2026-04-04 20:56:23 +0100

    weird-row-server: remove tangled's spindle instance

--- hosts/weird-row-server/tangled.nix
+++ hosts/weird-row-server/tangled.nix
@@ -18,7 +18,6 @@ in
 
   config = {
     local.ports.tangled-knot = 3003;
-    local.ports.tangled-spindle = 3004;
 
     services.tangled.knot = {
       enable = true;
@@ -32,24 +31,8 @@ in
       };
     };
 
-    services.tangled.spindle = {
-      enable = true;
-
-      server = {
-        listenAddr = "localhost:${config.local.ports.tangled-spindle.string}";
-        hostname = globals.domains.tangled-spindle;
-        owner = tangled-owner;
-      };
-    };
-
-    services.caddy = {
-      virtualHosts.${globals.domains.tangled-knot}.extraConfig = ''
-        reverse_proxy http://localhost:${config.local.ports.tangled-knot.string}
-      '';
-
-      virtualHosts.${globals.domains.tangled-spindle}.extraConfig = ''
-        reverse_proxy http://localhost:${config.local.ports.tangled-spindle.string}
-      '';
-    };
+    services.caddy.virtualHosts.${globals.domains.tangled-knot}.extraConfig = ''
+      reverse_proxy http://localhost:${config.local.ports.tangled-knot.string}
+    '';
   };
 }
