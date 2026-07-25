7908afc56bc989f2ff6f50084da0571bc4196492
Milo Moisson <milo@wiro.world>
2026-07-25 02:12:02 +0200

    lightweight: strip way more things from the profile

--- home-manager/profiles/lightweight.nix
+++ home-manager/profiles/lightweight.nix
@@ -1,5 +1,4 @@
 {
-  self,
   config,
   pkgs,
 
@@ -7,12 +6,7 @@
   ...
 }:
 
-let
-  inherit (self.inputs) stylix;
-in
 {
-  imports = [ ];
-
   config = {
     assertions = [
       {
@@ -33,40 +27,9 @@ in
       zellij.enable = true;
     };
 
-    home.sessionVariables = {
-      # Quick access to `~/Development` folder
-      DEV = "${config.home.homeDirectory}/Development";
-
-      # Would love to get rid of the Desktop folder
-      XDG_DESKTOP_DIR = "$HOME";
-
-      # Makes electron apps use ozone and not crash because xwayland is not there
-      NIXOS_OZONE_WL = "1";
-
-      # Respect XDG spec
-      BUN_INSTALL = "${config.xdg.dataHome}/bun";
-      CALCHISTFILE = "${config.xdg.cacheHome}/calc_history";
-      DOCKER_CONFIG = "${config.xdg.configHome}/docker";
-      HISTFILE = "${config.xdg.dataHome}/bash_history";
-      NPM_CONFIG_USERCONFIG = "${config.xdg.dataHome}/npm/npmrc";
-      RAD_HOME = "${config.xdg.dataHome}/radicle";
-      W3M_DIR = "${config.xdg.configHome}/w3m";
-      WAKATIME_HOME = "${config.xdg.configHome}/wakatime";
-    };
-
     home.packages = with pkgs; [
       # GUIs
-      cura-appimage
-      element-desktop
-      insomnia
-      mpv
       pavucontrol
-
-      # Needed for libreoffice spellchecking
-      hunspell
-      hunspellDicts.fr-moderne
-      hunspellDicts.en_US-large
-      hunspellDicts.en_GB-large
 
       # CLIs
       wf-recorder
@@ -79,11 +42,6 @@ in
       config = {
         style = "plain";
       };
-    };
-
-    programs.gpg = {
-      enable = true;
-      homedir = "${config.xdg.dataHome}/gnupg";
     };
 
     fonts.fontconfig.defaultFonts = {
